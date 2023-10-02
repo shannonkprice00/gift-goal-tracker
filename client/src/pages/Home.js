9202450899b89a07f78a0a0afe0ec7949697078f
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-10-02 12:10:48 -0500

    added welcome component and minimal styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Welcome from '../components';
 
 const Home = () => {
     return (
-        <main>
-            <div>Welcome!</div>
+        <main className='d-flex'>
+            <Welcome/>
         </main>
     )
 };
